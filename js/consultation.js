bd640451416481e4e29a2fe2bcd7dc99fc57598b
KampusNxt101 <kampusnxt@gmail.com>
2026-08-01 01:51:39 +0530

    Refactor consultation form submission logic

--- js/consultation.js
+++ js/consultation.js
@@ -1,3 +1,6 @@
+API_URL =
+"https://script.google.com/macros/s/AKfycbzkNJyxiTg6QN75C01VTQ0QHwiPhbC4LuptPoAcCX5ecUdmu1-wCOXrwkKzpLJLv_mI/exec";
+  
 document
 .getElementById("consultationFormData")
 .addEventListener("submit", async function (e) {
