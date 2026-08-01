b012b156b5231298833d314d52d28f83b90f59b1
KampusNxt101 <kampusnxt@gmail.com>
2026-08-02 01:26:07 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -31,7 +31,7 @@ contactForm.addEventListener("submit", async function (e) {
 
         const result = await response.text();
 
-        if (result === "Success") {
+        if (result.trim() === "Success") {
 
             status.style.color = "green";
             status.innerHTML =
