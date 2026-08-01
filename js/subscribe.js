c1ff5fa49d33b581ce52ce5921cb57dc189b45ef
KampusNxt101 <kampusnxt@gmail.com>
2026-08-01 17:37:25 +0530

    Replace WEB_APP_URL with API_URL from secrets

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,5 +1,6 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby5VyuaPCGO0qFKI12Z7TsvsXI9rfoNOemj40nsqSD-vIWno23g908nR2zFqn5eEnBN/exec"
+# # const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby5VyuaPCGO0qFKI12Z7TsvsXI9rfoNOemj40nsqSD-vIWno23g908nR2zFqn5eEnBN/exec"
 
+API_URL = secrets.GSheet_API_URL
 document
 .getElementById("subscribeForm")
 .addEventListener("submit", async function(e){
@@ -15,7 +16,7 @@ document
         formData.append("sheet","Subscribers");
         formData.append("email",email);
 
-        const response = await fetch(WEB_APP_URL,{
+        const response = await fetch(APP_URL,{
             method:"POST",
             body:formData
         });
