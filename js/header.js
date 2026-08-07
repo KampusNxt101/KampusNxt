32ed4fcc6757fe741c6129345d460af530093cd9
KampusNxt101 <kampusnxt@gmail.com>
2026-08-08 00:17:21 +0530

    Change header placeholder ID to 'header'

--- js/header.js
+++ js/header.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("header.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById("header-placeholder").innerHTML = data;
+            document.getElementById("header").innerHTML = data;
         })
         .catch(error => {
             console.error("Error loading header:", error);
