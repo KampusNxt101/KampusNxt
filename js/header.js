00d193714a5dffa445f6cb89de5f3009d474acd4
KampusNxt101 <kampusnxt@gmail.com>
2026-08-08 19:09:08 +0530

    Update header.js

--- js/header.js
+++ js/header.js
@@ -3,7 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch("header.html")
         .then(response => response.text())
         .then(data => {
-            document.getElementById("header").innerHTML = data;
+            document.getElementById("header").innerHTML = data;const menuToggle = document.getElementById("menu-toggle");
+            const navLinks = document.getElementById("nav-links");
+
+            if (menuToggle && navLinks) {
+
+                menuToggle.addEventListener("click", function () {
+                    navLinks.classList.toggle("active");
+                });
+
+            }
+
         })
         .catch(error => {
             console.error("Error loading header:", error);
